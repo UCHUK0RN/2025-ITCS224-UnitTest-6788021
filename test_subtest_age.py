a963b192d81af180157aa7884f9e37b99f1d64af
UCHUK0RN <uchukorn.eka@student.mahidol.ac.th>
2026-04-19 08:10:44 +0000

    UnitTest complete

---
+++ test_subtest_age.py
@@ -0,0 +1,22 @@
+import unittest
+from age import categorize_by_age
+
+class TestSubtestAge(unittest.TestCase):
+    
+    def test_child_age(self):
+        for age in range(0, 10): 
+            with self.subTest(age=age):
+                self.assertEqual(categorize_by_age(age), "Child")
+
+    def test_adolescent_age(self):
+        for age in range(10, 19): 
+            with self.subTest(age=age):
+                self.assertEqual(categorize_by_age(age), "Adolescent")
+
+    def test_adult_age(self):
+        for age in range(19, 66): 
+            with self.subTest(age=age):
+                self.assertEqual(categorize_by_age(age), "Adult")
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
